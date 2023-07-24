b655c7431527a1242d48886da8edf659f0bd8836
akarboush <akarboush@gmail.com>
2023-07-24 11:42:06 +0200

    Add project files.

---
+++ WpfSourceGen/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSourceGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public partial class MyViewModel : ObservableObject
+    {
+        [ObservableProperty] public string _firstname;
+
+        public MyViewModel()
+        {
+            Firstname = "1111";
+        }
+    }
+}
